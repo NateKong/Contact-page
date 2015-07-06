f1b37a543c962afa4c3bcbf48ee6d2b6ea78bfc1
Nate Kong <NateWKong@gmail.com>
2015-07-05 19:23:19 -0700

    contact page

--- contact.php
+++ contact.php
@@ -4,22 +4,22 @@
 		$name = $_POST['name'];
 	    $email = $_POST['email'];
 	    $message = $_POST['message'];
-		$header = 'From: natewkong@gmail.com';	
+		$header = 'From: messageSender@gmail.com';	
 		$body = "From: $name\nEmail: $email\n\nMessage:\n $message";
 		
-		$to = 'natewkong@gmail.com';
-		$subject = 'Nathanwkong.com website';
+		$to = 'messageReciever@gmail.com';
+		$subject = 'this is the subject in your email';
 	
 		//SENDS THE MESSAGE
 		if ( mail($to, $subject, $body, $header) )
 		{
 			echo '<script type="text/javascript">alert("Your message has been sent!");</script>';
-			echo "<script>setTimeout(\"location.href = 'http://www.nathanwkong.com';\",1500);</script>";
+			echo "<script>setTimeout(\"location.href = 'http://www.github.com/natekong';\",1500);</script>";
 		}
 		else
 		{
 			echo '<script type="text/javascript">alert("Oops, something went wrong, please try again");</script>';
-			echo "<script>setTimeout(\"location.href = 'http://www.nathanwkong.com/contactme';\",1500);</script>";
+			echo "<script>setTimeout(\"location.href = 'http://www.google.com';\",1500);</script>";
 		}
 	}
 ?>
